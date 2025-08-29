7f5f6f8e24fdf7bb27026036b28ec98bc5a98f4b
Ado <minexdt@gmail.com>
2025-08-29 13:18:45 -0600

    Actualizar owner-setprefixsubbots.js

--- plugins/owner-setprefixsubbots.js
+++ plugins/owner-setprefixsubbots.js
@@ -1,31 +1,64 @@
-let handler = async (m, { conn, text, isRowner, isSubowner }) => {
-  const emojip = '⚙️'
+import fs from 'fs'
+import path from 'path'
 
-  if (!isSubowner && !isRowner) 
-    return m.reply(`${emojip} Solo los *SubBots* o el *Dueño del Bot* pueden cambiar el prefijo.`)
+let handler = async (m, { conn, text, isRowner }) => {
+  const emojip = '⚙️'
 
   if (!text) 
     return m.reply(`${emojip} Por favor, proporciona un prefijo o lista de prefijos.\n
 > Ejemplo: #setprefix !
 > También puedes poner varios: #setprefix 🍉`)
 
-  if (text.toLowerCase() === 'multi') {
-    global.prefix = new RegExp('^[#$@*&?,;:+×!_\\-¿.]')
-    return m.reply(`${emojip} Prefijos activados en modo *MULTI-PREFIX*:  
-    > # $ @ * & ? , ; : + × ! _ - ¿ .`)
+  const senderNumber = m.sender.replace(/[^0-9]/g, '')
+  const botPath = path.join('./JadiBots', senderNumber)
+  const configPath = path.join(botPath, 'config.json')
+
+  
+  if (!fs.existsSync(botPath) && !isRowner) {
+    return m.reply(`${emojip} Este comando es sólo para los *SubBots* o el *Dueño del Bot*.`)
   }
 
+  let config = {}
+
+  if (fs.existsSync(configPath)) {
+    try {
+      config = JSON.parse(fs.readFileSync(configPath))
+    } catch (e) {
+      return m.reply('⚠️ Error al leer el config.json.')
+    }
+  }
+
+  if (text.toLowerCase() === 'multi') {
+    config.prefix = 'multi'
+    global.prefix = new RegExp('^[#$@*&?,;:+×!_\\-¿.]')
+    try {
+      fs.writeFileSync(configPath, JSON.stringify(config, null, 2))
+    } catch (err) {
+      console.error(err)
+      return m.reply('❌ Ocurrió un error al guardar el prefijo.')
+    }
+    return m.reply(`${emojip} Prefijos activados en modo *MULTI-PREFIX*:  
+> # $ @ * & ? , ; : + × ! _ - ¿ .`)
+  }
+
+  // Guardar prefijo normal
   let safe = [...text].map(c => c.replace(/([.*+?^${}()|\[\]\\])/g, '\\$1'))
+  config.prefix = text
 
   global.prefix = new RegExp('^(' + safe.join('|') + ')')
 
-  m.reply(`${emojip} El prefijo del bot ha sido cambiado a: *${text}*`)
+  try {
+    fs.writeFileSync(configPath, JSON.stringify(config, null, 2))
+    m.reply(`${emojip} El prefijo del bot ha sido cambiado a: *${text}*`)
+  } catch (err) {
+    console.error(err)
+    m.reply('❌ Ocurrió un error al guardar el prefijo.')
+  }
 }
 
 handler.help = ['setprefix']
 handler.tags = ['tools']
 handler.command = ['setprefix']
-handler.subowner = true
-handler.rowner = true
+
 
 export default handler
