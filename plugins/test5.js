3b312c98351a4302bf67425367bceb6d7a388bde
GianPoolS <116328472+GianPoolS@users.noreply.github.com>
2025-08-30 10:01:20 -0500

    Actualizar test5.js

--- plugins/test5.js
+++ plugins/test5.js
@@ -1,2 +1,25 @@
 //[##] Creado por GianPoolS
 //[##] No quites los créditos
+
+import fetch from 'node-fetch'
+
+let handler = async(m, { conn, args, usedPrefix, command }) => {
+
+fetch('https://raw.githubusercontent.com/GianPoolS/Mis-Archivos/refs/heads/main/doraemon.txt?token=GHSAT0AAAAAADKFZKB3FQPPLUSRTCRD24DC2FTCFAA').then(res => res.text()).then(body => {
+
+let randomkpop = body.split('\n')
+
+let randomkpopx = randomkpop[Math.floor(Math.random() * randomkpop.length)]
+conn.sendMessage(m.chat, { react: { text: '😁', key: m.key }})
+conn.sendButton(m.chat, `💟 Doraemon 💌`, namebot, randomkpopx, [['🔄 Next 🔄', `/${command}`]], m)
+
+})}
+
+handler.help = ['']
+
+handler.tags = ['']
+
+handler.command = ['t5']
+
+export default handler
+
