257d89c43a5f5cfb7ba67a13d4dda48fa3d47a7b
Ado <minexdt@gmail.com>
2025-08-30 21:45:38 -0600

    Actualizar _tts.js

--- plugins/_tts.js
+++ plugins/_tts.js
@@ -1,20 +1,29 @@
 import fetch from 'node-fetch'
+import yts from 'yt-search'
 
 let handler = async (m, { conn, args }) => {
-    if (!args[0]) return m.reply('✐ Pon el enlace de YouTube que quieres descargar en audio wey')
+    if (!args[0]) return m.reply('✐ Pon un nombre de canción o enlace de YouTube wey')
+
+    let query = args.join(' ')
+    let url
+
+    if (query.startsWith('http')) {
+        url = query
+    } else {
+        let search = await yts(query)
+        if (!search || !search.videos || !search.videos.length) return m.reply('✐ No encontré la canción wey')
+        url = search.videos[0].url
+    }
 
     try {
-        
-        let url = `https://myapiadonix.vercel.app/download/yt?url=${encodeURIComponent(args[0])}&format=mp3`
-
-        let res = await fetch(url)
+        let apiUrl = `https://myapiadonix.vercel.app/download/yt?url=${encodeURIComponent(url)}&format=mp3`
+        let res = await fetch(apiUrl)
         let json = await res.json()
 
         if (!json.status) return m.reply('✐ No se pudo descargar el audio wey')
 
         let { title, download } = json.result
 
-        
         await conn.sendMessage(m.chat, {
             audio: { url: download },
             mimetype: 'audio/mpeg',
