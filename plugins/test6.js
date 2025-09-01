340c7af47208194dd2eb91f3b0702bcd00e359ea
GianPoolS <116328472+GianPoolS@users.noreply.github.com>
2025-08-31 22:58:27 -0500

    Actualizar test6.js

--- plugins/test6.js
+++ plugins/test6.js
@@ -1,34 +1,44 @@
 import fs from "fs"
 
 let handler = async (m, { conn }) => {
-  let fkontak = {key:{remoteJid:"120363000000000000@g.us",fromMe:false,id:"MichiBot-MD",participant:"0@s.whatsapp.net"},message:{conversation:"⭐ MichiBot-MD ⭐"}}
+  let fkontak = {
+    key: { 
+      remoteJid: "120363000000000000@g.us", 
+      fromMe: false, 
+      id: "MichiBot-MD", 
+      participant: "0@s.whatsapp.net" 
+    },
+    message: { conversation: "⭐ MichiBot-MD ⭐" }
+  }
 
   const listMessage = {
-    text: `✨ *Menú de Comandos* ✨`,
-    footer: namebot,
-    title: "📖 Selecciona una categoría:",
-    buttonText: "Menu Lista",
-    sections: [
-      {
-        title: "📌 Información",
-        rows: [
-          { title: "🤖 Velocidad del Bot", rowId: ".p" },
-        ]
-      },
-      {
-        title: "👑 Creador y Colaboradores",
-        rows: [
-          { title: "👑 Contacto de los Creadores", rowId: ".owner" },
-        ]
-      },
-      {
-        title: "🎉 Extras",
-        rows: [
-          { title: "🎵 Audios de YT", rowId: ".play" },
-          { title: "🔧 Menu Lista", rowId: ".menulist" }
-        ]
-      }
-    ]
+    listMessage: {
+      title: "📖 Selecciona una categoría:",
+      description: "✨ *Menú de Comandos* ✨",
+      footerText: "⭐ MichiBot-MD ⭐",
+      buttonText: "Menu Lista",
+      sections: [
+        {
+          title: "📌 Información",
+          rows: [
+            { title: "🤖 Velocidad del Bot", rowId: ".p" },
+          ]
+        },
+        {
+          title: "👑 Creador y Colaboradores",
+          rows: [
+            { title: "👑 Contacto de los Creadores", rowId: ".owner" },
+          ]
+        },
+        {
+          title: "🎉 Extras",
+          rows: [
+            { title: "🎵 Audios de YT", rowId: ".play" },
+            { title: "🔧 Menu Lista", rowId: ".menulist" }
+          ]
+        }
+      ]
+    }
   }
 
   await conn.sendMessage(m.chat, { react: { text: '📂', key: m.key } })
