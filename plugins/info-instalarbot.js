932b4194a837a46ea69d56151bf64e9c1148440e
GianPoolS <116328472+GianPoolS@users.noreply.github.com>
2025-08-31 20:38:41 -0500

    Actualizar info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -80,7 +80,7 @@ ESCRIBE LOS SIGUIENTES COMANDOS UNO POR UNO:
           previewType: 'pdf',
           title: 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
           body: namebot,
-          thumbnail: buffer,
+          thumbnail: buffer,//fs.readFileSync('./storage/img/menu.jpg'),
           sourceUrl: 'https://youtu.be/nUSEEmlZw2g/'
         }
       },
