36b3e0b6f1bfdf6c701434581f7ec9c5a4bc8e1c
GianPoolS <116328472+GianPoolS@users.noreply.github.com>
2025-09-02 01:17:18 -0500

    Actualizar test4.js

--- plugins/test4.js
+++ plugins/test4.js
@@ -1,52 +1,99 @@
-const handler = async (m, { conn }) => {
-  let txt = '';
-try {    
-  const groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats);
-  const totalGroups = groups.length;
-  for (let i = 0; i < groups.length; i++) {
-    const [jid, chat] = groups[i];
-    const groupMetadata = ((conn.chats[jid] || {}).metadata || (await conn.groupMetadata(jid).catch((_) => null))) || {};
-    const participants = groupMetadata.participants || [];
-    const bot = participants.find((u) => conn.decodeJid(u.id) === conn.user.jid) || {};
-    const isBotAdmin = bot?.admin || false;
-    const isParticipant = participants.some((u) => conn.decodeJid(u.id) === conn.user.jid);
-    const participantStatus = isParticipant ? '👤 Participante' : '❌ Ex participante';
-    const totalParticipants = participants.length;
-    txt += `*◉ Grupo ${i + 1}*
-    *➤ Nombre:* ${await conn.getName(jid)}
-    *➤ ID:* ${jid}
-    *➤ Admin:* ${isBotAdmin ? '✔ Sí' : '❌ No'}
-    *➤ Estado:* ${participantStatus}
-    *➤ Total de Participantes:* ${totalParticipants}
-    *➤ Link:* ${isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(jid) || '--- (Error) ---'}` : '--- (No admin) ---'}\n\n`;
-  }
-  m.reply(`*Lista de grupos del Bot* 🤖\n\n*—◉ Total de grupos:* ${totalGroups}\n\n${txt}`.trim());
-} catch {
-  const groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats);
-  const totalGroups = groups.length;
-  for (let i = 0; i < groups.length; i++) {
-    const [jid, chat] = groups[i];
-    const groupMetadata = ((conn.chats[jid] || {}).metadata || (await conn.groupMetadata(jid).catch((_) => null))) || {};
-    const participants = groupMetadata.participants || [];
-    const bot = participants.find((u) => conn.decodeJid(u.id) === conn.user.jid) || {};
-    const isBotAdmin = bot?.admin || false;
-    const isParticipant = participants.some((u) => conn.decodeJid(u.id) === conn.user.jid);
-    const participantStatus = isParticipant ? '👤 Participante' : '❌ Ex participante';
-    const totalParticipants = participants.length;    
-    txt += `*◉ Grupo ${i + 1}*
-    *➤ Nombre:* ${await conn.getName(jid)}
-    *➤ ID:* ${jid}
-    *➤ Admin:* ${isBotAdmin ? '✔ Sí' : '❌ No'}
-    *➤ Estado:* ${participantStatus}
-    *➤ Total de Participantes:* ${totalParticipants}
-    *➤ Link:* ${isBotAdmin ? '--- (Error) ---' : '--- (No admin) ---'}\n\n`;
-  }
-  m.reply(`*Lista de grupos del Bot* 🤖\n\n*—◉ Total de grupos:* ${totalGroups}\n\n${txt}`.trim());
- }    
+import { performance } from "perf_hooks";
+
+const handler = async (m, { conn, usedPrefix }) => {
+  const _uptime = process.uptime() * 1000;
+  const uptime = clockString(_uptime);
+  const totalreg = Object.keys(global.db.data.users).length;
+
+  const chats = Object.entries(conn.chats).filter(
+    ([id, data]) => id && data.isChats,
+  );
+  const groups = chats.filter(([id]) => id.endsWith("@g.us"));
+
+  const { restrict, antiCall, antiprivado, modejadibot } =
+    global.db.data.settings[conn.user.jid] || {};
+  const { autoread, gconly, pconly, self } = global.opts || {};
+
+  // Calcular velocidad (latencia)
+  const old = performance.now();
+  await conn.sendPresenceUpdate("composing", m.chat);
+  const neww = performance.now();
+  const speed = (neww - old).toFixed(2);
+
+  const info = `
+╠═〘 𝐈𝐍𝐅𝐎 𝐃𝐄𝐋 𝐁𝐎𝐓 〙 ═
+╠
+╠➥ [🤴🏻] Creador: ${author}
+╠➥ [#️⃣] Numero: *+52 1 999 209 5479*
+╠➥ [🌐] Navegador: *${browser}*
+╠➥ [🎳] Prefijo: *${usedPrefix}*
+╠➥ [🔐] Chats Privados: *${chats.length - groups.length}*
+╠➥ [🦜] Chats de Grupo: *${groups.length}* 
+╠➥ [💡] Chats Totales: *${chats.length}* 
+╠➥ [🚀] Tiempo Activo: *${uptime}*
+╠➥ [🎩] Usuarios: *${totalreg} números*
+╠➥ [🔋] Bateria: *${conn.battery
+  ? (conn.battery.live ? '🔌 𝙲𝚊𝚛𝚐𝚊𝚗𝚍𝚘...' : '⚡ 𝙳𝚎𝚜𝚌𝚘𝚗𝚎𝚌𝚝𝚊𝚍𝚘')
+  : '𝙳𝚎𝚜𝚌𝚘𝚗𝚘𝚌𝚒𝚍𝚘'}*
+╠➥ [☑️] Autoread: ${autoread ? "*activado*" : "*desactivado*"}
+╠➥ [❗] Restrict: ${restrict ? "*activado*" : "*desactivado*"} 
+╠➥ [💬] Pconly: ${pconly ? "*𝚊𝚌𝚝𝚒𝚟𝚊𝚍𝚘*" : "*𝚍𝚎𝚜𝚊𝚌𝚝𝚊𝚍𝚘*"}
+╠➥ [🏢] Gconly: ${gconly ? "*𝚊𝚌𝚝𝚒𝚟𝚊𝚍𝚘*" : "*𝚍𝚎𝚜𝚊𝚌𝚝𝚊𝚍𝚘*"}
+╠➥ [🌎] Modo: ${self ? "*𝚙𝚛𝚒𝚟𝚊𝚍𝚘*" : "*𝚙𝚞𝚋𝚕𝚒𝚌𝚘*"}
+╠➥ [💬] Antiprivado: ${antiprivado ? "*𝚊𝚌𝚝𝚒𝚟𝚊𝚍𝚘*" : "*𝚍𝚎𝚜𝚊𝚌𝚝𝚊𝚍𝚘*"}
+╠➥ [🤖] ModeJadibot: ${modejadibot ? "*𝚊𝚌𝚝𝚒𝚟𝚊𝚍𝚘*" : "*𝚍𝚎𝚜𝚊𝚌𝚝𝚊𝚍𝚘*"}
+╠➥ [📵] Antillamada: ${antiCall ? "*𝚊𝚌𝚝𝚒𝚟𝚊𝚍𝚘*" : "*𝚍𝚎𝚜𝚊𝚌𝚝𝚊𝚍𝚘*"}
+╠➥ [🪀] Versión de WhatsApp: *${vs}*
+╠➥ [🤖] Bots activos: *${users?.length || '0'}*
+╠➥ [👨‍🦯] 𝚅𝙴𝙻𝙾𝙲𝙸𝙳𝙰𝙳: *${speed} ms*
+╠
+╠═〘 ${namebot} 〙 ═
+`.trim();
+
+  const doc = [
+    "pdf",
+    "zip",
+    "vnd.openxmlformats-officedocument.presentationml.presentation",
+    "vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "vnd.openxmlformats-officedocument.wordprocessingml.document",
+  ];
+  const document = doc[Math.floor(Math.random() * doc.length)];
+
+  const Message = {
+    document: { url: `https://github.com/Ado-rgb/Michi-WaBot` },
+    mimetype: `application/${document}`,
+    fileName: `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
+    fileLength: 10000000,
+    pageCount: 200,
+    contextInfo: {
+      forwardingScore: 200,
+      isForwarded: true,
+      externalAdReply: {
+        mediaUrl: "https://github.com/Ado926",
+        mediaType: 2,
+        previewType: "pdf",
+        title: "ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ",
+        body: namebot,
+        thumbnail: imagen1,
+        sourceUrl: "https://github.com/GianPoolS",
+      },
+    },
+    caption: info,
+    footer: namebot,
+    headerType: 6,
+  };
+
+  conn.sendMessage(m.chat, Message, { quoted: m });
 };
-handler.help = ['grups', 'gruplist'];
-handler.tags = [''];
-handler.command = ['grups']
-handler.rowner = true;
-handler.private = true;
+
+handler.help = [""];
+handler.tags = [""];
+handler.command = ["infobot"];
 export default handler;
+
+function clockString(ms) {
+  const h = Math.floor(ms / 3600000);
+  const m = Math.floor(ms / 60000) % 60;
+  const s = Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(":");
+}
